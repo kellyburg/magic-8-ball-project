da6c043fa08327ad4a3dbecb1a6b1fa4d309d4d3
Kelly Burgess <kellyburgess91@gmail.com>
2018-09-05 16:40:45 -0700

    added shake effect

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ $(document).ready(function(){
     magicEightBall.listOfAnswers=["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.","My reply is no.", "My sources say no", "Outlook not so good.", "Very doubtful."];
 	$("#answer").hide();
     magicEightBall.shakeEightBall= function (question){
+		$("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+		
         var randomNumber=Math.floor(Math.random()*magicEightBall.listOfAnswers.length);
         var answer = magicEightBall.listOfAnswers[randomNumber];
-$("#answer").text(answer);
     };
-var askQuestion = function(){
+var askQuestion = function(){	
 	$("#answer").hide();
-	$("#answer").fadeIn(4000);
+	$("#8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/8side.png");
+setTimeout( function(){
 	var question=prompt("Ask a Yes/No Question");
-	magicEightBall.shakeEightBall(question);
+	magicEightBall.shakeEightBall(question);}, 500);
+	$("#answer").fadeIn(4000);
+	
 };
 	$("#questionButton").click(askQuestion);
     });
